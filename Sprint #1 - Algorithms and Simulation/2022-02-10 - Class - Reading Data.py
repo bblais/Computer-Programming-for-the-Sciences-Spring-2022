8a08bc6841e5db3a214201cbd1199f649a0a4e84
Brian Blais <bblais@gmail.com>
2022-02-10 13:13:33 -0500

    plotting

---
+++ Sprint #1 - Algorithms and Simulation/2022-02-10 - Class - Reading Data.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[2]:
+
+
+from sci378 import *
+
+
+# In[3]:
+
+
+import pandas as pd
+data=pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/boot/catsM.csv')
+data.describe()
+
+
+# In[4]:
+
+
+data
+
+
+# In[5]:
+
+
+x=data['Bwt']
+y=data['Hwt']
+
+plot(x,y,'ro')
+xlabel('Body Weight [kg]')
+ylabel('Heart Weight [g]')
+title('My Lovely Cats and Their Hearts')
+
+
+# In[ ]:
+
+
+
+
