e935bd9365cacd1d1682864cc472bfa4628622a4
Brian Blais <bblais@gmail.com>
2022-02-17 13:21:40 -0500

    more

--- Sprint #1 - Algorithms and Simulation/2022-02-17 - Misc for Demo.py
+++ Sprint #1 - Algorithms and Simulation/2022-02-17 - Misc for Demo.py
@@ -46,18 +46,65 @@ data=pd.read_csv('data/Orange.csv')
 data
 
 
-# In[15]:
+# In[17]:
 
 
 x=data[data['Tree']==1]['age']
 y=data[data['Tree']==1]['circumference']
 
-plot(x,y,'o')
+plot(x,y,'o',label="Bob")
 
 x=data[data['Tree']==2]['age']
 y=data[data['Tree']==2]['circumference']
 
-plot(x,y,'s')
+plot(x,y,'s',label="Sally")
+
+legend()
+
+
+# In[18]:
+
+
+x=data[data['Tree']==1]['age']
+y=data[data['Tree']==1]['circumference']
+
+plot(x,y,'o',label="Bob")
+
+x=data[data['Tree']==2]['age']
+y=data[data['Tree']==2]['circumference']
+
+plot(x,y,'s',label="Sally")
+
+legend(loc='upper right')
+
+
+# In[19]:
+
+
+get_ipython().run_line_magic('pinfo', 'legend')
+
+
+# In[21]:
+
+
+k=5
+g=3
+
+x=data[data['Tree']==1]['age']
+y=data[data['Tree']==1]['circumference']
+
+plot(x,y,'o',label=f"Bob k={k},g={g}")
+
+
+k=2
+g=9
+
+x=data[data['Tree']==2]['age']
+y=data[data['Tree']==2]['circumference']
+
+plot(x,y,'s',label=f"Sally k={k},g={g}")
+
+legend(loc='upper right')
 
 
 # In[ ]:
