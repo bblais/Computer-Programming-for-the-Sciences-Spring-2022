26a7a40449b31ae7715887305513f7f91c7d835b
Brian Blais <bblais@gmail.com>
2022-02-17 12:51:49 -0500

    organges

--- Sprint #1 - Algorithms and Simulation/2022-02-17 - Misc for Demo.py
+++ Sprint #1 - Algorithms and Simulation/2022-02-17 - Misc for Demo.py
@@ -34,6 +34,32 @@ figure(figsize=(3,10))
 plot(rand(100),'o')
 
 
+# In[11]:
+
+
+data=pd.read_csv('data/Orange.csv')
+
+
+# In[12]:
+
+
+data
+
+
+# In[15]:
+
+
+x=data[data['Tree']==1]['age']
+y=data[data['Tree']==1]['circumference']
+
+plot(x,y,'o')
+
+x=data[data['Tree']==2]['age']
+y=data[data['Tree']==2]['circumference']
+
+plot(x,y,'s')
+
+
 # In[ ]:
 
 
