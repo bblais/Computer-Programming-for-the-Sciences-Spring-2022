19999256d4c7902cc03313cef4f63dc8fff1757e
Brian Blais <bblais@gmail.com>
2022-02-24 15:35:57 -0500

    cklass

--- Sprint #2 - Dynamical Systems and Parameters/2022-02-24 Parameter Fits.py
+++ Sprint #2 - Dynamical Systems and Parameters/2022-02-24 Parameter Fits.py
@@ -36,12 +36,12 @@ y_data=data['y']
 plot(t_data,y_data,'o')
 
 
-# In[19]:
+# In[20]:
 
 
 sim=Simulation()
 sim.figsize=(8,4)
-sim.add("x'=a*x*(1-x/k)",1,plot=True)
+sim.add("x'=a*x*(1-x/k)",75,plot=True)
 sim.params(a=1,k=50)
 sim.add_data(t=t_data,x=y_data,plot=True)
 sim.run(50)
@@ -56,6 +56,39 @@ sim.add("x'=a*x*(1-x/k)",1,plot=True)
 sim.params(a=1.5,k=275)
 sim.add_data(t=t_data,x=y_data,plot=True)
 sim.run(50)
+
+
+# <img src="https://wikimedia.org/api/rest_v1/media/math/render/svg/eba353633616971f427b13e175bfbdb1b99bcff0">
+
+# In[21]:
+
+
+"α*x-β*x*y"
+
+
+# In[24]:
+
+
+data=pd.read_csv("https://raw.githubusercontent.com/bblais/Systems-Modeling-Spring-2015-Notebooks/master/data/Lynx%20and%20Hare%20Data/lynxhare.csv",
+                sep=";")
+data
+
+
+# In[25]:
+
+
+t=data['year']
+y1=data['hare']
+y2=data['lynx']
+
+
+# In[28]:
+
+
+figure(figsize=(8,4))
+plot(t,y1,'-o',label='hare')
+plot(t,y2,'-o',label='lynx')
+legend()
 
 
 # In[ ]:
