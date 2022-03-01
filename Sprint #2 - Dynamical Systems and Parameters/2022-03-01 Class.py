09359d647e7dbc7f340f115d2241f9097ce345b3
Brian Blais <bblais@gmail.com>
2022-03-01 13:07:52 -0500

    anotherx

--- Sprint #2 - Dynamical Systems and Parameters/2022-03-01 Class.py
+++ Sprint #2 - Dynamical Systems and Parameters/2022-03-01 Class.py
@@ -162,7 +162,7 @@ sim.run(50)
 plot(sim.t,sim.x)
 
 
-# In[28]:
+# In[32]:
 
 
 sim=Simulation()
@@ -172,9 +172,10 @@ sim.params(a=1.5,k=270)
 sim.run(50)
 
 plot(sim.t,sim.x)
+title("Untable -- runs away from FP when started a little bit off of the FP")
 
 
-# In[30]:
+# In[31]:
 
 
 sim=Simulation()
@@ -194,6 +195,96 @@ sim.run(50)
 
 plot(sim.t,sim.x)
 
+
+title("Stable -- approaches FP when started a little bit off of the FP")
+
+
+# ## Non-zero mean SHO
+
+# In[41]:
+
+
+sim=Simulation()
+sim.add("x'=v",30)
+sim.add("v'=-k*x/m",0)
+sim.params(k=2,m=1)
+sim.run(50)
+
+
+# In[44]:
+
+
+new_y_data=y_data[3:]
+new_t_data=t_data[3:]
+
+
+# In[46]:
+
+
+μ=median(new_y_data)
+min_t=min(new_t_data)
+plot(sim.t+min_t,sim.x+μ)
+
+plot(new_t_data,new_y_data,'o')
+
+
+# In[48]:
+
+
+sim=Simulation()
+sim.add("x'=v",30)
+sim.add("v'=-k*x/m",0)
+sim.params(k=2,m=1)
+sim.run(9,50)
+
+
+μ=median(new_y_data)
+plot(sim.t,sim.x+μ)
+
+plot(new_t_data,new_y_data,'o')
+
+
+# ## Data with dates
+
+# In[52]:
+
+
+from sci378 import *
+
+
+# In[51]:
+
+
+data=pd.read_csv('http://jooskorstanje.com/full_data_logistic.csv',sep=';')
+data
+
+
+# In[53]:
+
+
+date_to_float(data['date'])
+
+
+# In[54]:
+
+
+x=data['date']
+y=data['total_cases']
+plot(x,y,'-o')
+
+
+# In[58]:
+
+
+x=range(len(data))
+y=data['total_cases']
+plot(x,y,'-o')
+
+
+# In[57]:
+
+
+x
 
 
 # In[ ]:
