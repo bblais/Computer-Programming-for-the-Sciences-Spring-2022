2e64cfd2f896e30fdc63b6a6b640609476c6a4be
Brian Blais <bblais@gmail.com>
2022-03-08 13:06:31 -0500

    class mar 8

---
+++ Sprint #2 - Dynamical Systems and Parameters/2022-03-08 Class.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[2]:
+
+
+from pyndamics3 import Simulation
+
+
+# In[25]:
+
+
+sim=Simulation()
+sim.add("N'=r*N*(1-N/K)-H",1,plot=True)
+sim.params(r=1,K=50,H=.979)
+sim.run(600)
+
+
+# In[26]:
+
+
+sim=Simulation()
+sim.add("x'=v",1)
+sim.add("v'=a",0)
+sim.add("a=k*vm*exp(-k*t)")
+sim.params(k=3,vm=2)
+sim.run(600)
+
+
+# In[28]:
+
+
+import pandas as pd
+
+
+# In[30]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+pd.read_csv('c:/Users/bblais/Downloads/somefile.csv')
+
+
+# In[32]:
+
+
+pd.read_csv('logistic_sample_data/logistic_sample_data_0.csv')
+
+
+# In[34]:
+
+
+data=pd.read_csv('FISH_LAND.csv')
+data
+
+
+# In[38]:
+
+
+data=data[data['Species']=='Japanese seabass [Lateolabrax japonicus]']
+data
+
+
+# In[37]:
+
+
+t=data['Year']
+y=data['Value']
+plot(t,y,'o')
+
+
+# In[ ]:
+
+
+
+
