2da3d244cb3b27d601a369666839ddc0d869564e
Brian Blais <bblais@gmail.com>
2022-03-10 12:46:11 -0500

    names

--- Sprint #2 - Dynamical Systems and Parameters/2022-03-10 Class.py
+++ Sprint #2 - Dynamical Systems and Parameters/2022-03-10 Class.py
@@ -29,6 +29,36 @@ x=data['Americans with Cellular Service (%)']
 t=t-min(t)  # this data doesn't start at t=0, so adjust
 
 
+# In[53]:
+
+
+sim=Simulation()
+sim.add("x' = a*x*(1-x/K)",13)
+sim.params(a=.25,K=110)
+sim.run(20)
+
+plot(sim.t,sim.x,'g-')
+plot(t,x,'bo-')
+
+# plot the x=0 fixed point
+sim=Simulation()
+sim.add("x' = a*x*(1-x/K)",0)
+sim.params(a=.25,K=110)
+sim.run(20)
+plot(sim.t,sim.x,'r-')
+
+
+# plot the x=K fixed point
+sim=Simulation()
+sim.add("x' = a*x*(1-x/K)",110)
+sim.params(a=.25,K=110)
+sim.run(20)
+plot(sim.t,sim.x,'r-')
+
+xlabel('Time [years]')
+ylabel('Percent Cell Service')
+
+
 # In[52]:
 
 
@@ -73,10 +103,35 @@ annotate('$x=0$ fixed point (unstable)',(18,0),xytext=(12,10), arrowprops=dict(a
 annotate('$x=K$ fixed point (stable)',(1,109),xytext=(1,90), arrowprops=dict(arrowstyle="->"))
 
 
-# In[23]:
+# ## data and variable names
+
+# In[55]:
 
 
-get_ipython().run_line_magic('pinfo', 'annotate')
+# plot the x=K fixed point
+sim=Simulation()
+sim.add("bob' = a*bob*(1-bob/K)",13,plot=True)
+sim.params(a=.25,K=110)
+sim.run(20)
+
+
+# In[56]:
+
+
+sally=data['Year']
+frank=data['Americans with Cellular Service (%)']
+sally=sally-min(sally)  # this data doesn't start at t=0, so adjust
+
+
+# In[57]:
+
+
+# plot the x=K fixed point
+sim=Simulation()
+sim.add("bob' = a*bob*(1-bob/K)",13,plot=True)
+sim.params(a=.25,K=110)
+sim.add_data(t=sally,bob=frank,plot=True      )
+sim.run(20)
 
 
 # In[ ]:
