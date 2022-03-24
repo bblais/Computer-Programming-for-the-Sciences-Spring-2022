a6172d2db2eea7fcbf6da11b7bc1ada79e4787df
Brian Blais <bblais@gmail.com>
2022-03-24 07:47:01 -0400

    data a fitting

---
+++ Sprint #3 - Parameter Fitting and Scientific Questions/2022-03-24 - fitting custom functions.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[5]:
+
+
+from sci378 import *
+
+
+# In[6]:
+
+
+from lmfit import *
+
+
+# See: https://lmfit.github.io/lmfit-py/builtin_models.html  for more examples
+
+# In[7]:
+
+
+x_data,y_data=(array([-10.        ,  -8.94736842,  -7.89473684,  -6.84210526,
+         -5.78947368,  -4.73684211,  -3.68421053,  -2.63157895,
+         -1.57894737,  -0.52631579,   0.52631579,   1.57894737,
+          2.63157895,   3.68421053,   4.73684211,   5.78947368,
+          6.84210526,   7.89473684,   8.94736842,  10.        ]),
+ array([ 84.5439605 ,  79.96429095,  53.29446178,  15.49595419,
+         55.59294258,  57.74976334, -17.47795207,  19.03107704,
+        -31.19976545, -10.54910001,  17.56865009,   5.42434731,
+         -9.60203555,  21.4654494 ,  44.94972133,   6.80172884,
+         62.75223289,  58.16212626, 111.04251023,  83.52494712]))
+
+
+# In[8]:
+
+
+plot(x_data,y_data,'o')
+
+
+# ## Step 1 - define the function
+
+# In[17]:
+
+
+def quad(x,a=1,b=1,c=1):
+    return a*x**2 + b*x + c
+
+
+# ## Step 2 - define the model and construct the parameter list
+
+# In[18]:
+
+
+qmodel=Model(quad)   # from lmfit
+
+
+# In[19]:
+
+
+qmodel.param_names
+
+
+# In[20]:
+
+
+params=qmodel.make_params()
+params
+
+
+# ## Step 3 - modify the parameter list (min, max, etc...) as needed
+
+# In[13]:
+
+
+params['a']=Parameter("a",min=0,value=0.5)
+
+
+# ## Step 4 - do the fit, look at the parameter values (do they make sense?), etc...
+
+# In[21]:
+
+
+result = qmodel.fit(y_data, params, x=x_data)
+
+
+# In[22]:
+
+
+result
+
+
+# ## Step 5 - plot your date and the predictions of the model
+
+# In[23]:
+
+
+plot(x_data,y_data,'o')
+
+x_fake=linspace(-12,12,100)
+y_fake=result.eval(x=x_fake)
+plot(x_fake,y_fake,'-')
+
+
+# ## Sometimes the function already exists in lmfit - but might have different names for parameters
+# 
+# https://lmfit.github.io/lmfit-py/builtin_models.html
+
+# In[24]:
+
+
+qmodel=models.QuadraticModel()
+
+
+# In[25]:
+
+
+qmodel.param_names
+
+
+# In[26]:
+
+
+params=qmodel.make_params()
+
+
+# In[27]:
+
+
+result = qmodel.fit(y_data, params, x=x_data)
+
+
+# In[28]:
+
+
+plot(x_data,y_data,'o')
+
+x_fake=linspace(-12,12,100)
+y_fake=result.eval(x=x_fake)
+plot(x_fake,y_fake,'-')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# # Another Data Set
+# 
+
+# In[9]:
+
+
+x_data,y_data=(array([0.        , 0.12820513, 0.25641026, 0.38461538, 0.51282051,
+        0.64102564, 0.76923077, 0.8974359 , 1.02564103, 1.15384615,
+        1.28205128, 1.41025641, 1.53846154, 1.66666667, 1.79487179,
+        1.92307692, 2.05128205, 2.17948718, 2.30769231, 2.43589744,
+        2.56410256, 2.69230769, 2.82051282, 2.94871795, 3.07692308,
+        3.20512821, 3.33333333, 3.46153846, 3.58974359, 3.71794872,
+        3.84615385, 3.97435897, 4.1025641 , 4.23076923, 4.35897436,
+        4.48717949, 4.61538462, 4.74358974, 4.87179487, 5.        ]),
+ array([ 1.0428932 ,  0.89749902,  0.75670392,  0.64111299,  0.55718173,
+         0.52405064,  0.56088843,  0.5490807 ,  0.42480484,  0.47428914,
+         0.2934649 ,  0.34352106,  0.33185889,  0.27993667,  0.26003406,
+         0.27138304,  0.39834035,  0.28156451,  0.2572975 ,  0.27710445,
+         0.15969731,  0.22588548,  0.18106594,  0.15347512,  0.25385423,
+         0.20835157,  0.1964213 ,  0.09014651,  0.11874164,  0.14871592,
+         0.06117995,  0.19194054,  0.05546755,  0.10819372,  0.13157402,
+         0.16435838,  0.04113969, -0.03053311,  0.0710629 ,  0.08495973]))
+
+
+# In[ ]:
+
+
+
+
---
+++ Sprint #3 - Parameter Fitting and Scientific Questions/2022-03-24 Fitting Peaks.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[2]:
+
+
+from sci378 import *
+from lmfit import *
+
+
+# In[3]:
+
+
+data=pd.read_csv('data/peaks/sample0.csv')
+
+
+# In[4]:
+
+
+data.head()
+
+
+# In[7]:
+
+
+t=data['t']
+y=data['y']
+
+
+# In[8]:
+
+
+plot(t,y,'o')
+
+
+# In[10]:
+
+
+model=models.GaussianModel()
+model.param_names
+
+
+# In[11]:
+
+
+results=model.fit(y,x=t,amplitude=1e7,center=1,sigma=1)
+results
+
+
+# In[12]:
+
+
+x_fake=linspace(0,2,2000)
+y_fake=results.eval(x=x_fake)
+plot(t,y,'o')
+plot(x_fake,y_fake,'-')
+xlim([.9,1.3])
+
+
+# In[13]:
+
+
+results.best_values
+
+
+# ## You can get individual parameters by name
+
+# In[14]:
+
+
+A=results.params['amplitude'].value
+A
+
+
+# ## Now your turn - Goal: find the amplitudes for all of the peaks for the data in the peaks folder
+
+# In[ ]:
+
+
+
+
