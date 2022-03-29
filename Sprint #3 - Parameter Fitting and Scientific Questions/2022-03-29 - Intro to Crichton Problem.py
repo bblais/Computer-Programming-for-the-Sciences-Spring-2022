0b02ba18212ca57f83afd70f2f8a5caf93d0c303
Brian Blais <bblais@gmail.com>
2022-03-29 13:17:29 -0400

    class

--- Sprint #3 - Parameter Fitting and Scientific Questions/2022-03-29 - Intro to Crichton Problem.py
+++ Sprint #3 - Parameter Fitting and Scientific Questions/2022-03-29 - Intro to Crichton Problem.py
@@ -1,6 +1,49 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+
+
+# In[2]:
+
+
+from sci378 import *
+
+
+# In[3]:
+
+
+from lmfit import *
+
+
+# In[4]:
+
+
+data=pd.read_csv('data/station.csv')
+
+
+# In[5]:
+
+
+data
+
+
+# In[7]:
+
+
+x=data['YEAR']
+y=data['metANN']
+
+x=x[y<200]
+y=y[y<200]
+
+
+plot(x,y,'-o')
+
+
 # In[ ]:
 
 
