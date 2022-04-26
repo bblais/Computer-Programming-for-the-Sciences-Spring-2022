b98448f76592e1a99853e50617a972ebb41fa6f3
Brian Blais <bblais@gmail.com>
2022-04-26 13:22:17 -0400

    class

---
+++ Sprint #4 - Probability and Science/2022-04-26 Going through files.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from glob import glob
+
+
+# In[2]:
+
+
+ls data/1999aa/
+
+
+# In[3]:
+
+
+ls data/1999ac/
+
+
+# In[6]:
+
+
+filenames=glob('data/1999aa/*v_*.dat')+glob('data/1999ac/*v_*.dat')
+filenames
+
+
+# In[ ]:
+
+
+
+
